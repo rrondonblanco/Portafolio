c3283103c3a9dffd7edd8bf54535bdb87ec17bb8
Roberto <rrondon18@gmail.com>
2020-10-04 19:56:17 -0300

    agregando java y otros

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+
+$(function(){
+    
+    $("a").click(function(event){
+        if (this.hash !=="") {
+            event.preventDefault();
+
+            var gato = this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 2000, function(){
+                windows.location.hash = gato;
+            });
+        }  
+    });
+
+    $('[data-toggle="popover"]').popover();
+
+});
