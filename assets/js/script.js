992219c676da9c1c98a3fd905f15fd3303aae138
Roberto <rrondon18@gmail.com>
2020-10-05 20:23:35 -0300

    arreglos Java y mix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,39 @@
+$(function() {
 
+    $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+    ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000, function() {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { 
+            return false;
+            } else {
+            $target.attr('tabindex','-1');
+            $target.focus();
+            };
+        });
+        }
+    }
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    
+
+})
 
 $(".jumbotron").css({ height: $(window).height() + "px" });
 
